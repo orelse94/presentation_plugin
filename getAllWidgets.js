84fdca9445d36bfa2bef04dcbada7a01baa6cdbf
Dean Shub <dean.shub@sisense.com>
2017-04-22 19:13:39 +0300

    selection and cancellation enabled

--- getAllWidgets.js
+++ getAllWidgets.js
@@ -1,96 +1,117 @@
-// const $dom = prism.$injector.get('ux-controls.services.$dom');
-selectedWidgets = [];
+// var $dom = prism.$injector.get('ux-controls.services.$dom');
+var selectedWidgets = {};
 
-function selectWidget(widgetId){
-  let realWidgetId = widgetId.target.attributes[1].nodeValue;
-  console.log(['before',selectedWidgets]);
-  //
-  // if (selectedWidgets.indexOf(realWidgetId) !== 1)  {
-  //   selectedWidgets.push(realWidgetId)} else {selectedWidgets.splice(realWidgetId)}
-  selectedWidgets.push(realWidgetId);
-  let widgetTitle =
-  console.log(selectedWidgets);
+function selectWidget(){
+  var $this = $(this);
+  var id = $this.attr('widgetId');
 
-//   if (()=>{return selectedWidgets.indexOf(realWidgetId) } == '-1') {
-//     //do nothing
-//     console.log(['true',selectedWidgets]);
-//     selectedWidgets.splice(realWidgetId)
-//   } else {
-//     console.log('false');
-//     selectedWidgets.push(realWidgetId);
-//   }
-//   console.log(selectedWidgets);
+  if ($this.hasClass('selected')){
+    $this.removeClass('selected');
+    selectedWidgets[id]=undefined;
+  }else{
+    var dashboardId = $this.attr('dashboardId');
+    selectedWidgets[id]={
+      id:id,
+      dashboardId:dashboardId,
+    };
+    $this.addClass('selected');
+  }
+}
+
+function createOverlayMenu(overlayDiv){
+  var overlayMenu = $('<div class="presentation-overlay-menu"></div>');
+  var overlayMenuSave = $('<div class="presentation-overlay-menu-button">Save</div>');
+  var overlayMenuCancel = $('<div class="presentation-overlay-menu-button">Cancel</div>');
+  overlayMenuCancel.click(function(){
+    selectedWidgets={};
+    overlayDiv.remove();
+  });
+  overlayMenuSave.click(function(){
+    console.log(selectedWidgets);
+    selectedWidgets={};
+    overlayDiv.remove();
+  });
+  overlayMenu.append(overlayMenuCancel);
+  overlayMenu.append(overlayMenuSave);
+
+  return overlayMenu;
 }
 
 function createOverlay(){
-  var overlayDiv = $('<div id="presentationOverlay" class="presentation_overlay"></div>');
-  findPosition().map(function(widgetContainer){
+  console.clear();
+  var overlayDiv = $('<div id="presentationOverlay" class="presentation-overlay"></div>');
+  findWidgetsPositions().map(function(widgetContainer){
     overlayDiv.append(widgetContainer);
   });
+  overlayDiv.append(createOverlayMenu(overlayDiv));
   $('body').append(overlayDiv);
-  // console.log(widgetsPositions);
 }
 
 
-function widgetsDivMaker(width,height,left,top,widgetId){
-  var widgetContainer = $(`<div class="widgets" widgetId="${widgetId}" style="width:${width}px;height:${height}px;left:${left}px;top:${top}px;"></div>`);
+function widgetsDivMaker(sizing){
+  var width = sizing.width;
+  var height = sizing.height;
+  var left = sizing.position.left;
+  var top = sizing.position.top;
+  var widgetId = sizing.widgetId;
+  var dashboardId = sizing.dashboardId;
+
+  var widgetContainer = $('<div class="presentation-widget-container" dashboardId="'+dashboardId+'" widgetId="'+widgetId+'" style="width:'+width+'px;height:'+height+'px;left:'+left+'px;top:'+top+'px;"></div>');
   widgetContainer.click(selectWidget);
   return widgetContainer;
 }
 
-function findPosition() {
-  const widgets = $('widget');
-  const widgetsContainers = [];
-  const offsetColl = [];
+function findWidgetsPositions() {
+  var widgets = $('widget');
+  var widgetsContainers = [];
+  var offsetColl = [];
 
-  for (i=0;i < widgets.length; i ++) {
-    // console.log({i});
-    let sizings = {
+  for (var i=0;i < widgets.length; i ++) {
+    var sizing = {
       position: widgets.eq(i).offset(),
       width: widgets.eq(i).width(),
       height: widgets.eq(i).height(),
+      dashboardId: window.prism.activeDashboard.oid,
       widgetId: widgets.eq(i).attr('widgetid'),
-      // widgetTitle: widgets.eq(i).attr('title')
+      widgetTitle: widgets.eq(i).attr('title'),
     };
-    widgetsContainers.push(widgetsDivMaker(sizings.width,sizings.height,sizings.position.left,sizings.position.top,sizings.widgetId,sizings.widgetTitle));
-    offsetColl.push(sizings);
-  };
+    widgetsContainers.push(widgetsDivMaker(sizing));
+    offsetColl.push(sizing);
+  }
 
-  console.log({offsetColl});
   return widgetsContainers;
-  // return offsetColl;
-  // var positions = widgets.map(widget){}
-  console.log({selectedWidgets});
-
 }
 
-const presButton = {
+var presButton = {
   id: 'pres',
   caption: 'pres',
   desc: 'pres1',
   execute: function () {
-    const widgetsObj = {};
-    const widgetsArray = prism.activeDashboard.widgets.$$widgets;
-    const widgets = widgetsArray
-    .map((widget) => {
-      let id = widget._id;
-      let dashId = widget.dashboardid;
-      let title = widget.title;
-      let desc = widget.desc;
-      return {id,dashId,title,desc};
+    // var widgetsObj = {};
+    // var widgetsArray = window.prism.activeDashboard.widgets.$$widgets;
+    // var widgets = widgetsArray
+    // .map(function(widget) {
+    //   var id = widget._id;
+    //   var dashId = widget.dashboardid;
+    //   var title = widget.title;
+    //   var desc = widget.desc;
+    //   return {
+    //     id:id,
+    //     dashId:dashId,
+    //     title:title,
+    //     desc:desc,
+    //   };
+    //
+    // });
+    // window.O = $;
+    // widgetsObj.widgets = widgets;
+    // console.log(widgetsObj);
 
-    });
-    window.O = $;
-
-    widgetsObj.widgets = widgets;
-    console.log(widgetsObj);
-
-    // templateUrl: '/plugins/presentation_plugin/overlay.html',
     createOverlay();
   },
 };
 
-prism.on('beforemenu',function (event, args) {
+window.prism.on('beforemenu',function (event, args) {
   if (args.settings.name == 'dashboard') {
     args.settings.items.push(presButton);
   }
